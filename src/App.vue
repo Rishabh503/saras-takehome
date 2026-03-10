e566491f2047507e4d054ad7de7825846ad98adb
Rishabh503 <rishabhtripathi2022@gmail.com>
2026-03-10 21:53:37 +0530

    project setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import SearchBar from './components/SearchBar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-[#fdf9f9] flex justify-center  p-6">
+    <div class="w-full max-w-3xl mx-auto">
+      <h1 class="text-4xl font-bold text-[#11d493] m-6 text-center">What are You Looking for?</h1>
+      <p class="text-center text-gray-500">
+        Type your query and get the best results out there in just one click !!
+      </p>
+      <SearchBar />
+    </div>
+    
+  </div>
+</template>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref} from "vue"
+
+import { Search, ArrowRight } from "lucide-vue-next"
+const query=ref("")
+function onSearch(){
+    console.log("query is :" , query.value)
+}
+</script>
+
+<template>
+    <main>
+        <div class="w-full flex items-center rounded-xl mt-4 shadow-sm pl-4 border-green-200 bg-white ">
+            <Search class="mr-2 text-gray-300 w-5 h-5"/>
+            <input type="text" v-model="query" placeholder="Start typing your query here "
+            class="flex-1 text-gray-600 placeholder-gray-400 outline-none"
+            />
+        <button @click="onSearch"
+      class="ml-3 bg-green-400 hover:bg-green-500 text-white p-2 rounded-lg flex items-center justify-center"
+    >
+        <ArrowRight class="h-4"/>
+    </button>
+        </div>
+    </main>
+   
+</template>
+
+<style scoped>
+</style>
